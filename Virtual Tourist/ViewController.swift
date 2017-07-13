56340f6109afb2d1bd89d6fcedf3f08ddcb89bed
theGituser <sagopu@mac2837371e7142.home>
2017-07-12 20:41:50 -0600

    first commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Satyanarayana Gopu on 7/12/17.
+//  Copyright © 2017 Appfish. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
