aaff44e262843b66e825c9a860d0cc56675a8719
theGituser <sagopu@mac2837371e7142.home>
2017-07-14 01:12:19 -0600

    Add user location and Annotations

---
+++ Virtual Tourist/PinAnnoation.swift
@@ -0,0 +1,16 @@
+//
+//  PinAnnoation.swift
+//  Virtual Tourist
+//
+//  Created by Satyanarayana Gopu on 7/14/17.
+//  Copyright © 2017 Appfish. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class PinAnnotation : MKPointAnnotation{
+    
+
+    
+}
--- Virtual Tourist/ViewController.swift
+++ Virtual Tourist/ViewController.swift
@@ -9,21 +9,86 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
     
     @IBOutlet weak var mapView: MKMapView!
+    var locationManager : CLLocationManager!
+    var gesture : UILongPressGestureRecognizer!
+    override func viewWillLayoutSubviews() {
+        
+    }
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        mapView.showsUserLocation = true
+        gesture = UILongPressGestureRecognizer(target: self, action: #selector(gestured))
+        mapView.addGestureRecognizer(gesture)
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        if CLLocationManager.authorizationStatus() == .notDetermined{
+            
+            locationManager.requestAlwaysAuthorization()
+            
+        }
+        locationManager.requestLocation()
+    }
+    
+    func gestured(){
+        
+        let point = gesture.location(in: mapView)
+        let coordinate = mapView.convert(point, toCoordinateFrom: mapView)
+        let annoatation = MKPointAnnotation()
+        annoatation.coordinate = coordinate
+        mapView.addAnnotation(annoatation)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
+}
+
+extension ViewController : CLLocationManagerDelegate{
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        
+        
     }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        mapView.setCenter((locations.last?.coordinate)!, animated: true)
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
+        locationManager.stopUpdatingLocation()
+    }
+    
+}
 
-
+extension ViewController : MKMapViewDelegate{
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        let pinAnnotionView : MKPinAnnotationView!
+        
+        if let newPin = mapView.dequeueReusableAnnotationView(withIdentifier: "pin") as? MKPinAnnotationView{
+            
+            pinAnnotionView = newPin
+            
+        }
+        else{
+            
+            pinAnnotionView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "pin")
+            
+        }
+        pinAnnotionView.animatesDrop = true
+        return pinAnnotionView
+        
+    }
+    
+    
+    
 }
 
